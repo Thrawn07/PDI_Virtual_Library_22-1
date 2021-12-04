d422c483770b08065ebf0f42628d42c297f4886e
FernValSag <fernando.valencias@my.unitec.edu.mx>
2021-12-03 19:47:07 -0600

    Database finished

--- qrgenerator-python/databasetest.py
+++ qrgenerator-python/databasetest.py
@@ -26,11 +26,10 @@ def pic2blob(file):
         blob = f.read()
     return blob
 
-""""
 def insert_autor(conn):
     try:
         c = conn.cursor()
-        sql = """ """INSERT INTO AUTOR (APELLIDO, NOMBRE) VALUES 
+        sql = """INSERT INTO AUTOR (APELLIDO, NOMBRE) VALUES 
                                                         ('Tzu', 'Sun'), 
                                                         ('Balmes', 'Jaime'), 
                                                         ('Spurgeon', 'Charles'), 
@@ -51,17 +50,16 @@ def insert_autor(conn):
                                                         ('Verne', 'Julio'),
                                                         ('Hitler', 'Adolf'),
                                                         ('Wilde', 'Oscar');"""
-       """
         c.execute(sql)
         conn.commit()
     except Error as e:
-        print(e)"""
-"""
-def insert_categoria(conn, categoria):
+        print(e)
+
+def insert_categoria(conn):
     try:
         c = conn.cursor()
-        sql = """ """INSERT INTO CATEGORIAS (ID_CATEGORIA, NOMBRE) VALUES 
-                                                                (000, 'Generalidades'),
+        sql = """INSERT INTO CATEGORIA (ID_CATEGORIA, NOMBRE) VALUES 
+                                                                (0, 'Generalidades'),
                                                                 (100, 'Filosofía y Psicología'),
                                                                 (200, 'Religión'),
                                                                 (300, 'Ciencias Sociales'),
@@ -69,57 +67,57 @@ def insert_categoria(conn, categoria):
                                                                 (500, 'Ciencias Naturales y Matemáticas'),
                                                                 (600, 'Tecnología'),
                                                                 (700, 'Artes'),
-                                                                (800. 'Literatura y retórica')
+                                                                (800, 'Literatura y retórica'),
                                                                 (900, 'Geografía e Historia');"""
-    """c.execute(sql)
-        conn.commit()
-    except Error as e:
-        print(e)"""
-"""
-def insert_libro(conn, libro):
-    try:
-        c = conn.cursor()
-        sql = """ """INSERT INTO LIBRO (ID_LIBRO, TITULO, FECHA, 
-                                    AID_AUTOR, CID_CATEGORIA, 
-                                    QID_CATEGORIA) VALUES 
-                                                    (101, 'El arte de la guerra', 'Siglo V a. C.', 1, 100, 1,)
-                                                    (102, 'El criterio', 1845, 2, 100, 2,) 
-                                                    (201, 'El tesoro de David', 2015, 3, 200, 3,)
-                                                    (801, 'Alicia en el país de las Maravillas', 1865, 4, 800, 4,)
-                                                    (301, 'El contrato social', 1762, 5, 300, 5,)
-                                                    (401, 'Gramática castellana', 1492, 6, 400, 6,)
-                                                    (501, 'EL origen de las especies', 1859, 7, 500, 7,) 
-                                                    (601, 'CSS avanzado', 2005, 8, 600, 8,)
-                                                    (701, 'Lenguaje musical II', 2006, 9, 700, 9,)
-                                                    (802, 'Frankestein', 1818, 10, 800, 10,)
-                                                    (901, 'Historia de la revolución rusa', 1930, 11, 900, 11,)
-                                                    (803, 'Dagón', 1917, 12, 800, 12,)
-                                                    (804, 'El gato negro', 1843, 13, 800, 13,)
-                                                    (805, 'Don Quijote de la mancha', 1605, 14, 800, 14,)
-                                                    (602, 'La Divina comedia', 1321, 15, 700, 15,)
-                                                    (806, 'El sabueso de los Baskerville', 1902, 16, 800, 16,)
-                                                    (807, 'El lobo Estepario', 1927, 17, 800, 17,)
-                                                    (808, 'Viaje al centro de la tierra', 1864, 18, 800, 18,)
-                                                    (103, 'Mi lucha', 1925, 19, 100, 19,)
-                                                    (809, 'El fantasma de Canterville', 1887, 20, 800, 20,);"""
-  """      c.execute(sql, libro)
+        c.execute(sql)
         conn.commit()
     except Error as e:
         print(e)
-"""
 
+def insert_libro(conn):
+    try:
+        c = conn.cursor()
+        sql =  """INSERT INTO LIBRO (ID_LIBRO, TITULO, FECHA, 
+                                    AID_AUTOR, CID_CATEGORIA, 
+                                    QID_QR) VALUES 
+                                            (101, 'El arte de la guerra', 400, 1, 100, 1),
+                                            (102, 'El criterio', 1845, 2, 100, 2),
+                                            (201, 'El tesoro de David', 2015, 3, 200, 3),
+                                            (801, 'Alicia en el país de las Maravillas', 1865, 4, 800, 4),
+                                            (301, 'El contrato social', 1762, 5, 300, 5),
+                                            (401, 'Gramática castellana', 1492, 6, 400, 6),
+                                            (501, 'EL origen de las especies', 1859, 7, 500, 7), 
+                                            (601, 'CSS avanzado', 2005, 8, 600, 8),
+                                            (701, 'Lenguaje musical II', 2006, 9, 700, 9),
+                                            (802, 'Frankestein', 1818, 10, 800, 10),
+                                            (901, 'Historia de la revolución rusa', 1930, 11, 900, 11),
+                                            (803, 'Dagón', 1917, 12, 800, 12),
+                                            (804, 'El gato negro', 1843, 13, 800, 13),
+                                            (805, 'Don Quijote de la mancha', 1605, 14, 800, 14),
+                                            (602, 'La Divina comedia', 1321, 15, 700, 15),
+                                            (806, 'El sabueso de los Baskerville', 1902, 16, 800, 16),
+                                            (807, 'El lobo Estepario', 1927, 17, 800, 17),
+                                            (808, 'Viaje al centro de la tierra', 1864, 18, 800, 18),
+                                            (103, 'Mi lucha', 1925, 19, 100, 19),
+                                            (809, 'El fantasma de Canterville', 1887, 20, 800, 20);"""
+        c.execute(sql)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+"""
 def insert_qr(ID_QR, IMAGEN):
     try:
         conn = sqlite3.connect('library.db')
         c = conn.cursor()
-        sql = """INSERT INTO QR (ID_QR, IMAGEN) VALUES (?, ?);"""
-
+        sql =""" """INSERT INTO QR (ID_QR, IMAGEN) VALUES (?, ?);"""
+"""
         picture = pic2blob(IMAGEN)
         data_tuple = (ID_QR, picture)
         c.execute(sql, data_tuple)
         conn.commit()
     except Error as e:
-        print(e)
+        print(e)"""
 
 def main():
     database = r"library.db"
@@ -167,12 +165,12 @@ def main():
         create_table(conn, sql_create_libro_table)
 
         ####    LLENADO DE TABLAS   ####
-        """
+        
         insert_autor(conn)
         insert_categoria(conn)
         insert_libro(conn)
-        """
-        insert_qr(1, "img/1.png")
+        
+        """insert_qr(1, "img/1.png")
         insert_qr(2, "img/2.png")
         insert_qr(3, "img/3.png")
         insert_qr(4, "img/4.png")
@@ -190,7 +188,7 @@ def main():
         insert_qr(17, "img/17.png")
         insert_qr(18, "img/18.png")
         insert_qr(19, "img/19.png")
-        insert_qr(20, "img/20.png")
+        insert_qr(20, "img/20.png")"""
 
     else:
         print("Error! No se puede crear la conexión a la base de datos")
